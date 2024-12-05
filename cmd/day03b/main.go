5916e2da070eb2c2a8035034bba18fee54d42e25
Gerard Kok <gerard@gerardkok.nl>
2024-12-05 22:59:16 +0100

    cleanup

--- cmd/day03b/main.go
+++ cmd/day03b/main.go
@@ -5,7 +5,6 @@ import (
 	"bytes"
 	"os"
 	"path/filepath"
-	"regexp"
 
 	"adventofcode2024/internal/day"
 	"adventofcode2024/internal/projectpath"
@@ -15,26 +14,23 @@ type Day03b struct {
 	day.DayInput
 }
 
-var (
-	mulRE     = regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)$`)
-	tokenRE   = regexp.MustCompile(`do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)`)
-	enabledRE = regexp.MustCompile(`(?s)do\(\).*?don't\(\)`)
-)
-
 func NewDay03b(inputFile string) Day03b {
 	return Day03b{day.DayInput(inputFile)}
 }
 
 func parseNumberBackwards(data []byte, startAt int) (int, int) {
 	result := 0
-	for i, j := startAt, 1; i >= 0 && j <= 1000; i, j = i-1, j*10 {
+
+	pow := 1
+	for i := startAt; i >= 0 && i >= startAt-3; i-- {
 		c := data[i]
 		if c < '0' || c > '9' {
-			//fmt.Printf("returning number: %d\n", result)
 			return result, i
 		}
-		result += int(c-'0') * j
+		result += int(c-'0') * pow
+		pow *= 10
 	}
+
 	return 0, 0
 }
 
@@ -47,9 +43,7 @@ func mul(data []byte) int {
 
 	left, i := parseNumberBackwards(data, i-1)
 
-	if i-3 >= 0 && bytes.Equal(data[i-3:i+1], []byte("mul(")) {
-		// fmt.Printf("bytes: %s\n", string(data[i-3:i+1]))
-		//fmt.Printf("returning %d * %d\n", left, right)
+	if i >= 3 && bytes.Equal(data[i-3:i+1], []byte("mul(")) {
 		return left * right
 	}
 
@@ -90,20 +84,7 @@ func (d Day03b) Part1() int {
 
 	for scanner.Scan() {
 		line := scanner.Bytes()
-		// line2 := make([]byte, len(line))
-		// copy(line2, line)
 
-		// m1 := 0
-
-		// if match := mulRE.Find(line); match != nil {
-		// 	m := string(match[4 : len(match)-1])
-
-		// 	l, r, _ := strings.Cut(m, ",")
-		// 	left, _ := strconv.Atoi(l)
-		// 	right, _ := strconv.Atoi(r)
-		// 	fmt.Printf("m1 left: %d, m1 right: %d\n", left, right)
-		// 	m1 = left * right
-		// }
 		sum += mul(line)
 	}
 
@@ -113,10 +94,6 @@ func (d Day03b) Part1() int {
 func (d Day03b) Part2() int {
 	file, _ := os.Open(string(d.DayInput))
 	defer file.Close()
-
-	// f, _ := os.Create("myprogram.prof")
-	// pprof.StartCPUProfile(f)
-	// defer pprof.StopCPUProfile()
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(splitAtClosingBracket)
@@ -133,38 +110,15 @@ func (d Day03b) Part2() int {
 		} else if bytes.HasSuffix(line, []byte("don't()")) {
 			enabled = false
 		} else if enabled {
-			// if match := mulRE.Find(line); match != nil {
-			// 	m := string(match[4 : len(match)-1])
-
-			// 	l, r, _ := strings.Cut(m, ",")
-			// 	left, _ := strconv.Atoi(l)
-			// 	right, _ := strconv.Atoi(r)
-			// 	sum += left * right
-			// }
 			sum += mul(line)
 		}
 	}
-
-	// 	switch {
-	// 	case match == "do()":
-	// 		enabled = true
-	// 	case match == "don't()":
-	// 		enabled = false
-	// 	case enabled:
-	// 		m := match[4 : len(match)-1]
-
-	// 		l, r, _ := strings.Cut(m, ",")
-	// 		left, _ := strconv.Atoi(l)
-	// 		right, _ := strconv.Atoi(r)
-	// 		sum += left * right
-	// 	}
-	// }
 
 	return sum
 }
 
 func main() {
-	d := NewDay03b(filepath.Join(projectpath.Root, "cmd", "day03b", "aoc-2024-day-03-challenge-2.txt"))
+	d := NewDay03b(filepath.Join(projectpath.Root, "cmd", "day03b", "input.txt"))
 
 	day.Solve(d)
 }
