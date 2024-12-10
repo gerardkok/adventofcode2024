b148407b92237db967c0a35081cd03366deaec1b
Gerard Kok <gerard@gerardkok.nl>
2024-12-10 20:16:09 +0100

    progress on day 9

--- cmd/day09b/main.go
+++ cmd/day09b/main.go
@@ -20,65 +20,70 @@ type day09 struct {
 	day.DayInput
 }
 
-type disk struct {
-	blocks []int
-	free   int
+type block struct {
+	id   int
+	free bool
 }
+
+type disk []block
 
 func NewDay09(opts ...day.Option) day09 {
 	return day09{day.NewDayInput(path, opts...)}
 }
 
 func parseInput(input []byte) disk {
-	var blocks []int
-	free := 0
+	var blocks []block
 
 	for i, c := range bytes.TrimSpace(input) {
-		var id int
+		id := 0
 		if i%2 == 0 {
 			id = i / 2
-		} else {
-			id = -1
-			free += int(c - '0')
 		}
-		blocks = append(blocks, slices.Repeat([]int{id}, int(c-'0'))...)
+
+		b := block{id, i%2 == 1}
+
+		blocks = append(blocks, slices.Repeat([]block{b}, int(c-'0'))...)
 	}
 
-	return disk{blocks, free}
+	return blocks
 }
 
-func (d disk) compact() disk {
-	for l, r := 0, len(d.blocks)-1; l < r; l, r = l+1, r-1 {
+func (d disk) compact() {
+	for l, r := 0, len(d)-1; ; l, r = l+1, r-1 {
 		// find file
-		for d.blocks[r] == -1 {
+		for d[r].free {
 			r--
 		}
 
 		// find free block
-		for d.blocks[l] != -1 {
+		for !d[l].free {
 			l++
 		}
 
-		d.blocks[l] = d.blocks[r]
-	}
+		if l >= r {
+			break
+		}
 
-	return disk{d.blocks[:len(d.blocks)-d.free], 0}
+		d[l], d[r] = d[r], d[l]
+	}
 }
 
 func (d disk) checksum() int {
 	result := 0
 
-	for i, b := range d.blocks {
-		result += i * b
+	for i, b := range d {
+		result += i * b.id
 	}
 
 	return result
 }
 
 func (d disk) print() {
-	fmt.Printf("free: %d\n", d.free)
-	for i, b := range d.blocks {
-		fmt.Printf("[%5d] %d\n", i, b)
+	fmt.Printf("free: ?\n")
+	for i, b := range d {
+		if b.id > 0 {
+			fmt.Printf("[%4d] %d\n", i, b.id)
+		}
 	}
 }
 
@@ -87,9 +92,9 @@ func (d day09) Part1() int {
 
 	disk := parseInput(input)
 
-	compacted := disk.compact()
+	disk.compact()
 
-	return compacted.checksum()
+	return disk.checksum()
 }
 
 func (d day09) Part2() int {
