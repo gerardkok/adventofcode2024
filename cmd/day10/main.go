5c3320271f5961cf8b38a21b34605c541eb0f696
Gerard Kok <gerard@gerardkok.nl>
2024-12-11 13:26:52 +0100

    improving day 10

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -19,7 +19,7 @@ type day10 struct {
 	day.DayInput
 }
 
-type grid [][]int
+type grid [][]byte
 
 type position struct {
 	x, y int
@@ -37,19 +37,24 @@ func NewDay10(opts ...day.Option) day10 {
 
 func parseGrid(input []string) grid {
 	result := make(grid, len(input)+2)
-	result[0] = slices.Repeat([]int{-1}, len(input[0])+2)
-	result[len(input)+1] = slices.Repeat([]int{-1}, len(input[0])+2)
+	result[0] = slices.Repeat([]byte{'#'}, len(input[0])+2)
+	result[len(input)+1] = slices.Repeat([]byte{'#'}, len(input[0])+2)
 	for x, line := range input {
-		result[x+1] = slices.Repeat([]int{-1}, len(input[0])+2)
-		for y, c := range line {
-			result[x+1][y+1] = int(c - '0')
-		}
+		result[x+1] = []byte("#" + line + "#")
 	}
 	return result
 }
 
-func (g grid) height(p position) int {
+func (g grid) height(p position) byte {
 	return g[p.x][p.y]
+}
+
+func (g grid) trailhead(p position) bool {
+	return g.height(p) == '0'
+}
+
+func (g grid) trailend(p position) bool {
+	return g.height(p) == '9'
 }
 
 func (p position) to(d direction) position {
@@ -57,11 +62,12 @@ func (p position) to(d direction) position {
 }
 
 func (g grid) ends(p position) map[position]struct{} {
-	if g.height(p) == 9 {
+	if g.trailend(p) {
 		return map[position]struct{}{p: {}}
 	}
 
 	result := make(map[position]struct{})
+
 	for _, dir := range directions {
 		next := p.to(dir)
 		if g.height(next) == g.height(p)+1 {
@@ -74,11 +80,12 @@ func (g grid) ends(p position) map[position]struct{} {
 }
 
 func (g grid) rating(p position) int {
-	if g.height(p) == 9 {
+	if g.trailend(p) {
 		return 1
 	}
 
 	result := 0
+
 	for _, dir := range directions {
 		next := p.to(dir)
 		if g.height(next) == g.height(p)+1 {
@@ -89,6 +96,15 @@ func (g grid) rating(p position) int {
 	return result
 }
 
+func (g grid) walk(fn func(position)) {
+	for x := range len(g) {
+		for y := range len(g[0]) {
+			p := position{x, y}
+			fn(p)
+		}
+	}
+}
+
 func (d day10) Part1() int {
 	lines := d.ReadLines()
 
@@ -96,15 +112,12 @@ func (d day10) Part1() int {
 
 	result := 0
 
-	for x := range len(grid) {
-		for y := range len(grid[0]) {
-			p := position{x, y}
-			if grid.height(p) == 0 {
-				n := grid.ends(p)
-				result += len(n)
-			}
+	grid.walk(func(p position) {
+		if grid.trailhead(p) {
+			n := grid.ends(p)
+			result += len(n)
 		}
-	}
+	})
 
 	return result
 }
@@ -116,14 +129,11 @@ func (d day10) Part2() int {
 
 	result := 0
 
-	for x := range len(grid) {
-		for y := range len(grid[0]) {
-			p := position{x, y}
-			if grid.height(p) == 0 {
-				result += grid.rating(p)
-			}
+	grid.walk(func(p position) {
+		if grid.trailhead(p) {
+			result += grid.rating(p)
 		}
-	}
+	})
 
 	return result
 }
