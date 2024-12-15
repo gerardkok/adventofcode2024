2d52e3c7a41c78fc0c23c2c8b937ee6fb81f904b
Gerard Kok <gerard@gerardkok.nl>
2024-12-15 09:39:40 +0100

    tweaks to day 10

--- internal/conv/conv.go
+++ internal/conv/conv.go
@@ -1,5 +1,7 @@
 package conv
 
+// convenience
+
 import "strconv"
 
 func MustAtoi(s string) int {
@@ -7,5 +9,15 @@ func MustAtoi(s string) int {
 	if err != nil {
 		panic(err)
 	}
+	return result
+}
+
+func SumFunc[T any](s []T, fn func(T) int) int {
+	result := 0
+
+	for _, e := range s {
+		result += fn(e)
+	}
+
 	return result
 }
